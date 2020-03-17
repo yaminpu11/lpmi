4098820b43b89c14f6f1913da94ec3a4b2fb4ed1
Ahmad Kashabul Yamin <ahmad.yamin@podomorouniversity.ac.id>
2020-03-17 16:41:12 +0700

    LPMI Template

---
+++ application/views/template/V_home.php
@@ -0,0 +1,514 @@
+<!--Carousel Wrapper-->
+<div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-2" data-slide-to="1"></li>
+    <li data-target="#carousel-example-2" data-slide-to="2"></li>
+  </ol>
+  <!--/.Indicators-->
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <div class="view  ">
+        <img class="d-block w-100 " height="500px" src="http://via.placeholder.com/640x360"
+          alt="First slide">
+        <div class="mask rgba-black-light"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Light mask</h3>
+<!--         <p>First text</p> -->
+      </div>
+    </div>
+    <div class="carousel-item">
+      <!--Mask color-->
+      <div class="view">
+        <img class="d-block w-100 " height="500px" src="http://via.placeholder.com/640x360"
+          alt="Second slide">
+        <div class="mask rgba-black-light"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Strong mask</h3>
+<!--         <p>Secondary text</p> -->
+      </div>
+    </div>
+    <div class="carousel-item">
+      <!--Mask color-->
+      <div class="view">
+        <img class="d-block w-100 " height="500px" src="http://via.placeholder.com/640x360"
+          alt="Third slide">
+        <div class="mask rgba-black-light"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Slight mask</h3>
+<!--         <p>Third text</p> -->
+      </div>
+    </div>
+  </div>
+  <!--/.Slides-->
+  <!--Controls-->
+  <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+    <span  aria-hidden="true"><i class="fas fa-caret-left fa-3x"></i></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+    <span  aria-hidden="true"><i class="fas fa-caret-right fa-3x"></i></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <!--/.Controls-->
+</div>
+<!--/.Carousel Wrapper-->
+
+
+<section class="dark-grey">
+<div class="container py-5">
+  <div class="row">
+    <div class="col text-center">
+    <h1 class="font-weight-bold dark-grey-text my-4 pb-2 text-center">About Us</h1>
+    <hr class="w-header bg-blue-hr mb-4">
+      <p class="grey-text text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <button type="button" class="btn btn-outline-indigo btn-rounded waves-effect mb-2 ">Read More <i class="fas fa-angle-double-right"></i></button>
+    </div>
+  </div>
+  <!--Grid row-->
+</div>
+</section>
+
+
+<div class="container">
+    <hr class="w-100">
+</div>
+
+<!-- News-->
+<section class="">
+  <div class="container py-5">
+  <!-- Section heading -->
+    <h1 class="font-weight-bold dark-grey-text my-2 pb-2 text-center">News</h1>
+    <hr class="w-header bg-blue-hr">
+    <p class="text-center mb-4"> Lorem ipsum dolor sit amet</p>
+  <!-- Section description -->
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+
+    <div class="col-lg-6 col-md-12">
+
+      <!-- Featured news -->
+      <div class="single-news mb-lg-0 mb-4">
+
+        <!-- Image -->
+        <div class="view overlay rounded z-depth-1-half mb-4">
+          <img class="img-fluid w-100" src="http://via.placeholder.com/640x360" alt="Sample image">
+          <a>
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+
+        <!-- Data -->
+        <div class="news-data d-flex justify-content-between">
+          <a href="#!" class="deep-primary-text">
+            <h6 class="font-weight-bold"><i class="fas fa-user pr-2"></i>Admin</h6>
+          </a>
+          <p class="font-weight-bold dark-grey-text"><i class="fas fa-clock-o pr-2"></i>27/02/2018</p>
+        </div>
+
+        <!-- Excerpt -->
+        <h3 class="font-weight-bold dark-grey-text mb-3"><a>Title of the news</a></h3>
+        <p class="grey-text mb-lg-0 mb-md-5 mb-4 ">Sed ut perspiciatis unde voluptatem omnis iste natus
+          error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+          inventore veritatis et quasi architecto beatae vitae explicabo. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
+
+      </div>
+      <!-- Featured news -->
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-6 col-md-12">
+
+      <!-- Small news -->
+      <div class="single-news mb-4">
+
+        <!-- Grid row -->
+        <div class="row">
+
+          <!-- Grid column -->
+          <div class="col-md-3">
+
+            <!--Image-->
+            <div class="view overlay rounded z-depth-1 mb-4">
+              <img class="img-fluid" src="http://via.placeholder.com/640x360" alt="Sample image">
+              <a>
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-9">
+
+            <!-- Excerpt -->
+            <p class="font-weight-bold dark-grey-text">26/02/2018</p>
+            <div class="d-flex justify-content-between">
+              <div class="col-11 text-truncate pl-0 mb-3">
+                <a href="#!" class="grey-text">At vero eos et accusamus et iusto odio dignissimos
+                  ducimus qui blanditiis</a>
+              </div>
+              <a><i class="fas fa-angle-double-right"></i></a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+      </div>
+      <!-- Small news -->
+
+      <!-- Small news -->
+      <div class="single-news mb-4">
+
+        <!-- Grid row -->
+        <div class="row">
+
+          <!-- Grid column -->
+          <div class="col-md-3">
+
+            <!--Image-->
+            <div class="view overlay rounded z-depth-1 mb-4">
+              <img class="img-fluid" src="http://via.placeholder.com/640x360"
+                alt="Sample image">
+              <a>
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-9">
+
+            <!-- Excerpt -->
+            <p class="font-weight-bold dark-grey-text">25/02/2018</p>
+            <div class="d-flex justify-content-between">
+              <div class="col-11 text-truncate pl-0 mb-3">
+                <a href="#!" class="grey-text">Itaque earum rerum hic tenetur a sapiente delectus</a>
+              </div>
+              <a><i class="fas fa-angle-double-right"></i></a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+      </div>
+      <!-- Small news -->
+
+      <!-- Small news -->
+      <div class="single-news mb-4">
+
+        <!-- Grid row -->
+        <div class="row">
+
+          <!-- Grid column -->
+          <div class="col-md-3">
+
+            <!--Image-->
+            <div class="view overlay rounded z-depth-1 mb-4">
+              <img class="img-fluid" src="http://via.placeholder.com/640x360" alt="Sample image">
+              <a>
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-9">
+
+            <!-- Excerpt -->
+            <p class="font-weight-bold dark-grey-text">24/02/2018</p>
+            <div class="d-flex justify-content-between">
+              <div class="col-11 text-truncate pl-0 mb-3">
+                <a href="#!" class="grey-text">Soluta nobis est eligendi optio cumque nihil impedit quo
+                  minus</a>
+              </div>
+              <a><i class="fas fa-angle-double-right"></i></a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+      </div>
+      <!-- Small news -->
+
+      <!-- Small news -->
+      <div class="single-news">
+
+        <!-- Grid row -->
+        <div class="row">
+
+          <!-- Grid column -->
+          <div class="col-md-3">
+
+            <!--Image-->
+            <div class="view overlay rounded z-depth-1 mb-md-0 mb-4">
+              <img class="img-fluid" src="http://via.placeholder.com/640x360" alt="Sample image">
+              <a>
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-9">
+
+            <!-- Excerpt -->
+            <p class="font-weight-bold dark-grey-text">23/02/2018</p>
+            <div class="d-flex justify-content-between">
+              <div class="col-11 text-truncate pl-0 mb-lg-3">
+                <a href="#!" class="grey-text">Duis aute irure dolor in reprehenderit in voluptate</a>
+              </div>
+              <a><i class="fas fa-angle-double-right"></i></a>
+            </div>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+      </div>
+      <!-- Small news -->
+
+    </div>
+    <!--Grid column-->
+
+  </div>
+  <!-- Grid row -->
+  </div>
+</section>
+<!-- News -->
+
+
+
+
+<div class="view jarallax" style="height: 15vh;background-attachment:fixed; ">
+<section class="jarallax-img" style="background-image: url('<?= base_url('assets/img/bg_podomoro.jpg')?>'); background-repeat: no-repeat; background-size: cover; background-position: center center;background-attachment:fixed; ">
+  
+  <div class="mask rgba-black-strong py-5">
+
+    <div class="container text-center my-5">
+
+      <h3 class="font-weight-bold text-center white-text pb-2">Portal Podomoro</h3>
+      <hr class="w-header hr-light my-4">
+      <p class="lead text-center white-text pt-2 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+
+      <a id="play" class="btn-floating btn-default btn-lg" data-toggle="modal" data-target="#modal1"><i class="fas fa-link"></i></a>
+
+    </div>
+
+  </div>
+
+</section>
+</div>
+
+
+
+<div id="carousel-example-multi" class="carousel slide carousel-multi-item v-2 py-5" data-ride="carousel">
+  <h1 class="font-weight-bold black-text mb-4 pb-2 text-center">Recommended Jobs</h1>
+  <hr class="w-header bg-blue-hr">
+  <!--Controls-->
+  <div class="controls-top control-center">
+    <a class="btn-floating btn-default float-left left-30x" href="#carousel-example-multi" data-slide="prev"><i
+        class="fas fa-chevron-left"></i></a>
+    <a class="btn-floating btn-default float-right right-30x" href="#carousel-example-multi" data-slide="next"><i
+        class="fas fa-chevron-right"></i></a>
+  </div>
+
+  <div class="carousel-inner v-2 pt-2" role="listbox">
+
+    <div class="carousel-item active">
+      <div class="col-12 col-md-2">
+        <div class="card card-wrapper mb-2">
+          <div class="card-up" style="background-image: url(<?= base_url('assets/img/jobs/LOGO-AGUNG-PODOMORO-LAND.png')?>); background-size: contain; background-position: center;background-repeat: no-repeat;">
+          
+          </div>
+
+          <div class="card-body mx-2">
+            <p class="card-title font-weight-bold">Marketing</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+              card's content.</p>
+            <a class="btn btn-default btn-md btn-rounded float-right">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+
+      <div class="col-12 col-md-2">
+        <div class=" card card-wrapper mb-2">
+          <div class="card-up" style="background-image: url(<?= base_url('assets/img/jobs/logo-centralpark.png')?>); background-size: contain; background-position: center;background-repeat: no-repeat;">
+          
+          </div>
+          <div class="card-body mx-2">
+            <p class="card-title font-weight-bold">Programer</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+              card's content.</p>
+            <a class="btn btn-default btn-md btn-rounded float-right">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="col-12 col-md-2">
+        <div class="card card-wrapper mb-2">
+          <div class="card-up" style="background-image: url(<?= base_url('assets/img/jobs/logo-neo-soho.png')?>); background-size: contain; background-position: center;background-repeat: no-repeat;">
+         
+          </div>
+          <div class="card-body mx-2">
+            <p class="card-title font-weight-bold">Digital Marketing</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the  card's content.</p>
+            <a class="btn btn-default btn-md btn-rounded float-right">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="col-12 col-md-2">
+        <div class="card card-wrapper mb-2">
+          <div class="card-up" style="background-image: url(<?= base_url('assets/img/jobs/logo-neo-soho.png')?>); background-size: contain; background-position: center;background-repeat: no-repeat;">
+            
+          </div>
+          <div class="card-body mx-2">
+            <p class="card-title font-weight-bold">Manajer</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+              card's content.</p>
+            <a class="btn btn-default btn-md btn-rounded float-right">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="col-12 col-md-2">
+        <div class="card card-wrapper mb-2">
+          <div class="card-up" style="background-image: url(http://localhost/podivers/assets/img/jobs/podomoro-university-logo.com.png); background-size: contain; background-position: center;background-repeat: no-repeat;">
+            
+          </div>
+          <div class="card-body mx-2">
+            <p class="card-title font-weight-bold">Supervision</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+              card's content.</p>
+            <a class="btn btn-default btn-md btn-rounded float-right">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+  </div>
+
+</div>
+
+
+<section class="grey lighten-2 py-5">
+  
+  <div class="flex-center">
+
+    <!--First row-->
+    <div class="row">
+
+      <!--First column-->
+      <div class="col-md-3 flex-center">
+        <img src="https://mdbootstrap.com/img/Photos/Template/34.png" class="img-fluid wow fadeIn" data-wow-delay=".2s">
+      </div>
+      <!--/First column-->
+
+      <!--Second column-->
+      <div class="col-md-3 flex-center">
+        <img src="https://mdbootstrap.com/img/Photos/Template/35.png" class="img-fluid wow fadeIn" data-wow-delay=".4s">
+      </div>
+      <!--/Second column-->
+
+      <!--Third column-->
+      <div class="col-md-3 flex-center">
+        <img src="https://mdbootstrap.com/img/Photos/Template/36.png" class="img-fluid wow fadeIn" data-wow-delay=".4s">
+      </div>
+      <!--/Third column-->
+
+      <!--Fourth column-->
+      <div class="col-md-3 flex-center">
+        <img src="https://mdbootstrap.com/img/Photos/Template/37.png" class="img-fluid wow fadeIn" data-wow-delay=".2s">
+      </div>
+      <!--/Fourth column-->
+
+    </div>
+    <!--/First row-->
+
+  </div>
+  
+</section>
+
+
+<div class="container pt-5 my-5 ">
+  <section class="p-md-3 mx-md-5 text-center">
+    <h2 class="text-center mx-auto font-weight-bold mb-4 pb-2">HIMA</h2>
+    <hr class="w-header bg-blue-hr">
+    <div class="row py-5">
+     
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+        <div class="p-4 ">
+          <div class="card-image " style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);border-radius:50%">
+
+            <a href="#!">
+              <div class="text-white d-flex h-100 mask  rgba-green-strong py-4 rounded-circle">
+                
+                <div class="second-content align-self-center mx-auto text-center">
+                 <img src="<?= base_url('assets/img/logohima/ARC-P.png')?>"
+              class="img-fluid rounded-circle card-image" />
+                </div>
+              </div>
+            </a>
+
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+        <div class="p-4 ">
+          <div class="card-image " style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);border-radius:50%">
+
+            <a href="#!">
+              <div class="text-white d-flex h-100 mask rgba-yellow-strong py-4 rounded-circle">
+                
+                <div class="second-content align-self-center mx-auto text-center">
+                 <img src="<?= base_url('assets/img/logohima/ACC-P.png')?>"
+              class="img-fluid rounded-circle card-image" />
+                </div>
+              </div>
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+</div>
